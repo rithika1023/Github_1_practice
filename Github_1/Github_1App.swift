8ad13a0f867da40b55053e4ca0bd118a92f51330
Rithika Manchala <manchalarithika@gmail.com>
2023-06-12 10:23:21 -0500

    Initial Commit

---
+++ Github_1/Github_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Github_1App.swift
+//  Github_1
+//
+//  Created by Ravi Manchala on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
